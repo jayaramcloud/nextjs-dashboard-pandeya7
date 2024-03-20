98a8c4d46cfc4c654a9fc5b5e09eae7f3632b000
jayaramcloud <jayaram.linux@gmail.com>
2024-03-20 07:25:02 -0600

    updated on mar 20

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  // This is equivalent to in fetch(..., {cache: 'no-store'})...
   noStore();
 
   try {
